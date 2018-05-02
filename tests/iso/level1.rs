d3ce46c40f63f0a980c845b69ceb46eb487ba6c0
Martin Larralde <martin.larralde@ens-cachan.fr>
2018-05-02 18:18:25 +0200

    Develop ISO level 1 tests in several independent test cases

--- tests/iso/level1.rs
+++ tests/iso/level1.rs
@@ -1,27 +1,38 @@
 use std::io::Read;
+use std::path::PathBuf;
 
 use opticaldisc::iso::IsoFs;
 
-#[test]
-fn test_iso_level1() {
+const path: PathBuf = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+    .join("static")
+    .join("iso")
+    .join("alpine.level1.iso");
 
-    let mut path = ::std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    path.push("static");
-    path.push("iso");
-    path.push("alpine.level1.iso");
+#[test]
+fn test_is_dir() {
 
     let mut iso = IsoFs::from_path(path).unwrap();
-
     assert!(iso.is_dir("ETC"));
+    assert!(iso.is_dir("/ETC"));
     assert!(iso.is_dir("ETC/APK"));
-    assert!(iso.is_file("ETC/APK/ARCH"));
+    assert!(iso.is_dir("/ETC/APK"));
     assert!(!iso.is_dir("ETC/NODIR"));
+    assert!(!iso.is_dir("ETC/APK/ARCHI"));
+}
 
-    let arch = iso.metadata("/ETC/APK/ARCH").unwrap();
-    assert!(arch.is_file());
-
+/// Checks the last directory record is parsed, i.e. the parser does stop
+/// as intended before reaching the start of the data blocks.
+#[test]
+fn test_last_record() {
     assert!(iso.is_dir("SBIN"));
     assert!(iso.is_file("SBIN/APK"));
     assert!(iso.is_file("SBIN/LDCONFIG"));
     assert!(iso.is_file("SBIN/MKMNTDIR"));
+}
+
+#[test]
+fn test_metadata() {
+    let arch = iso.metadata("/ETC/APK/ARCH").unwrap();
+    assert!(arch.is_file());
+    assert!(!arch.is_dir());
 }
